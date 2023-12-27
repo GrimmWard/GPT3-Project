58616eab3e1c174725bb0c8c435a24da081a60b9
GrimmWard <vanakramar76@gmail.com>
2023-12-27 18:59:18 +0200

    add possobility section & fix whatgpt3

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -26,11 +26,11 @@ const Features = () => {
       <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
       <p>Request Early Access to Get Started</p>
     </div>
-    {/* <div className="gpt3__features-container">
+    <div className="gpt3__features-container">
       {featuresData.map((item, index) => (
         <Feature title={item.title} text={item.text} key={item.title + index} />
       ))}
-    </div> */}
+    </div>
   </div>;
 };
 
